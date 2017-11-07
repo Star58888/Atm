575c0966706e9e092cc0d39e5d87162ed1baccbb
Star58888 <s0913940588@gmail.com>
2017-11-07 22:20:12 +0800

    updata

--- app/src/main/java/com/star/atm/MainActivity.java
+++ app/src/main/java/com/star/atm/MainActivity.java
@@ -48,6 +48,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        int id =item.getItemId();
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
