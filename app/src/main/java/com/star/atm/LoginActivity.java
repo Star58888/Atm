9984cb7e160e80a2650b580e271258adba71c351
Star58888 <s0913940588@gmail.com>
2017-11-07 21:35:22 +0800

    Initial commit

---
+++ app/src/main/java/com/star/atm/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.star.atm;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public void login(View view) {
+        EditText edUserid = (EditText)findViewById(R.id.userid);
+        EditText edPasswd = (EditText)findViewById(R.id.passwd);
+        String uid = edUserid.getText().toString();
+        String pw = edPasswd.getText().toString();
+        if (uid.equals("jack") && pw.equals("1234")) {
+            SharedPreferences setting = getSharedPreferences("atm" , MODE_PRIVATE);
+            setting.edit().putString("PREF_USERID", uid).commit();
+            Toast.makeText(this , " 登入成功" ,Toast.LENGTH_SHORT).show();
+            getIntent().putExtra("LOGIN_USERID" , uid);
+            getIntent().putExtra("LOGIN_PASSWORD" , pw);
+            setResult(RESULT_OK , getIntent());
+            finish();
+        }
+        else {
+            new AlertDialog.Builder(this).setTitle("ATM").setMessage("登入失敗")
+                    .setPositiveButton("OK" , null).show();
+        }
+    }
+
+    public void cancel(View view) {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        EditText edUserid = (EditText) findViewById(R.id.userid);
+        SharedPreferences setting = getSharedPreferences("atm" , MODE_PRIVATE);
+        edUserid.setText(setting.getString("PREF_USERID",""));
+    }
+}
---
+++ app/src/main/java/com/star/atm/MainActivity.java
@@ -0,0 +1,51 @@
+package com.star.atm;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean logon = false;
+    public static final int FUNC_LOGON = 1;  // 登入功能
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_OK) {
+            String uid = data.getStringExtra("LOGIN_USERID");
+            String pw = data.getStringExtra("LOGIN_PASSWORD");
+            Log.d("RESULT" , uid + "/" + pw);
+        } else {
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (!logon) {
+            Intent intent = new Intent(this , LoginActivity.class);
+//
+            startActivityForResult(intent , FUNC_LOGON);
+
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu ,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
